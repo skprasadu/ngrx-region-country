5ef5523754c6389136b2be33c9d6f17ee50c14d1
Krishna Prasad <praskrishna@gmail.com>
2022-03-28 05:29:46 -0700

    Added documentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,13 +29,20 @@ export class AppComponent {
   }
 
   ngOnInit() {
+    //For quick and dirty solution, I am loading it ngOnInit in real life example, we have do as 
+    //below using NgRX Action/ Effect, as example here,
+    //https://stackblitz.com/edit/angular-wglqco?file=src%2Fapp%2Fproduct.effect.ts
     this.http.get("assets/data.json").subscribe(data =>{
+      //load countries data
       this.countries = data;
-      console.log(this.countries);
+      
+      //load regions data
       this.regions = [...new Set(this.countries.map((x: { region: string; }) => x.region))];
-      console.log(this.regions);
-      this.getData(this.regions[0]);
+      
+      //on load select 1st region
+      this.getSelectedRegion(this.regions[0]);
 
+      //load all default observables from NgRX
       this.selectValCountry$ = this.store.select(state => state.selectVal.selectedCountry);
       this.selectValRegion$ = this.store.select(state => state.selectVal.selectedRegion);
       this.selectedCountryName$ = this.store.select(state => state.selectVal.selectedCountryObj.name);
@@ -47,12 +54,15 @@ export class AppComponent {
     });
   }
 
-  getData(selectedRegion: any) {
-    let selectedCountriesBasedOnRegion = this.countries
+  getSelectedRegion(selectedRegion: any) {
+    const selectedCountriesBasedOnRegion = this.countries
                       .filter((x: { region: string; }) => x.region == selectedRegion)
                       .map((x: { name: any; }) => x.name);
+
+    //Dispatch selected regions and all the countries based on select regions to ngrx
     this.store.dispatch(selectValueRegion({selectedRegion, selectedCountriesBasedOnRegion}));
                       console.log(selectedRegion, selectedCountriesBasedOnRegion);
+    
     this.getSelectedCountry(selectedCountriesBasedOnRegion[0]);
   }
 
@@ -60,7 +70,7 @@ export class AppComponent {
     let selectedCountryObj = this.countries
                       .filter((x: { name: string; }) => x.name == selectedCountry)[0];
 
+    //Dispatch selected countries and the country based on select country to ngrx
     this.store.dispatch(selectValueCountry({selectedCountry, selectedCountryObj}));
-    console.log(selectedCountry, selectedCountryObj);
   }
 }
